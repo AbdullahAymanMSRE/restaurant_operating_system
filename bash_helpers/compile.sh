df4e8936efe66124035e5b94aebe1b2c9b36dff1
Abdullah Ayman <abdullahaymanmsre@gmail.com>
2025-01-11 01:46:12 +0200

    fix compile file

--- bash_helpers/compile.sh
+++ bash_helpers/compile.sh
@@ -5,9 +5,9 @@ if [ ! -d "c_output" ]; then
   mkdir c_output
 fi
 
-gcc -c c_code/shared.c -pthread
-gcc -c c_code/order_process.c -pthread
-gcc c_code/client.c shared.o order_process.o -o c_output/client -pthread
-gcc c_code/staff.c shared.o order_process.o -o c_output/staff -pthread
-gcc c_code/display.c shared.o order_process.o -o c_output/display -pthread
-gcc c_code/init_memory.c shared.o -o c_output/init_memory -pthread
+gcc -c c_code/shared.c -o c_output/shared.o -pthread
+gcc -c c_code/order_process.c -o c_output/order_process.o -pthread
+gcc c_code/client.c c_output/shared.o c_output/order_process.o -o c_output/client -pthread
+gcc c_code/staff.c c_output/shared.o c_output/order_process.o -o c_output/staff -pthread
+gcc c_code/display.c c_output/shared.o c_output/order_process.o -o c_output/display -pthread
+gcc c_code/init_memory.c c_output/shared.o -o c_output/init_memory -pthread
