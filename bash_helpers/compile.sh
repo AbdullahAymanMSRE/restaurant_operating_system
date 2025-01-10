4b134554d278a1efcb6f7babc14c5c2f500041d6
Abdullah Ayman <abdullahaymanmsre@gmail.com>
2025-01-11 01:40:13 +0200

    fix compile file

--- bash_helpers/compile.sh
+++ bash_helpers/compile.sh
@@ -1,6 +1,13 @@
-gcc -c ../c_code/shared.c -pthread
-gcc -c ../c_code/order_process.c -pthread
-gcc ../c_code/client.c shared.o order_process.o -o ../c_output/client -pthread
-gcc ../c_code/staff.c shared.o order_process.o -o ../c_output/staff -pthread
-gcc ../c_code/display.c shared.o order_process.o -o ../c_output/display -pthread
-gcc ../c_code/init_memory.c shared.o -o ../c_output/init_memory -pthread
+#!/bin/bash
+
+# Create output directory if it does not exist
+if [ ! -d "c_output" ]; then
+  mkdir c_output
+fi
+
+gcc -c c_code/shared.c -pthread
+gcc -c c_code/order_process.c -pthread
+gcc c_code/client.c shared.o order_process.o -o c_output/client -pthread
+gcc c_code/staff.c shared.o order_process.o -o c_output/staff -pthread
+gcc c_code/display.c shared.o order_process.o -o c_output/display -pthread
+gcc c_code/init_memory.c shared.o -o c_output/init_memory -pthread
